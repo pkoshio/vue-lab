2ae35af2a3b6bf415f3e3bb8b9ada87fe0b9e1c6
pkoshio <pkoshio0788@gmail.com>
2020-10-25 21:05:25 +0900

    Exercise 1~3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h3>テキストバインディング</h3>
+    <span>{{ msg }}</span>
+    <h3>レストレンダリング</h3>
+    <ul id="item-list">
+      <li v-for="item in items" :key="item.id">
+        {{ item.name }}
+      </li>
+    </ul>
+    <h3>イベントハンドリング</h3>
+    <button v-on:click="onClick">実行</button>
   </div>
 </template>
+<style scoped>
+.home {
+  text-align: left;
+}
+</style>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+declare interface Item {
+  id: number;
+  name: string;
+}
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      msg: "Hello Vue.js!",
+      items: [
+        {
+          id: 1,
+          name: "keyboard"
+        },
+        {
+          id: 2,
+          name: "mouse"
+        }
+      ] as Item[]
+    };
   },
+  methods: {
+    onClick(): void {
+      alert(this.msg);
+    }
+  }
 });
 </script>
